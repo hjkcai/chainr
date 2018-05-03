c5344b95a4fbe5e6948858f155830e1f17a3926b
Jingkun Hua <huajingkun1214@gmail.com>
2018-05-03 22:40:52 +0800

    chore: remove node esm bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import typescript from 'rollup-plugin-typescript2'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
+  external: ['util'],
   plugins: [
     nodeResolve(),
     typescript()
   ],
   output: {
+    exports: 'named',
     sourcemap: true
   }
 }
